151c8098692dfc58ccdff7c6dbd6afdcd0c6fe7f
Julien <hu.julien.ts4@gmail.com>
2020-12-10 14:11:41 +0100

    add prints to results folder, test function, both datasets

--- project2/main.py
+++ project2/main.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 
 import torch
 import torch.nn as nn
@@ -6,11 +7,13 @@ import torch.optim as optim
 import torchvision.models as models
 from torch.utils import data
 
-from data.dataLoader_real_dataset import Dataset
-from model.architectures.resnet import resnet101
+from data.dataLoader_synthetic_dataset import Dataset as SyntheticDataset
+from data.dataLoader_real_dataset import Dataset as RealDataset
 
 
 num_classes = 13
+
+results_folder = "results"
 
 def get_model(model_name):
     if model_name == 'resnet_3d':
@@ -19,8 +22,6 @@ def get_model(model_name):
         net = models.video.mc3_18(pretrained=False, num_classes=num_classes)
     elif model_name == 'resnet_2_1d':
         net = models.video.r2plus1d_18(pretrained=False, num_classes=num_classes)
-    elif model_name == 'resnet_101':
-        net = resnet101(num_classes=num_classes)
     else:
         sys.exit('Error: Incorrect model name')
     return net
@@ -35,7 +36,6 @@ def train(model, device, train_loader, optimizer, loss_func, epoch, model_name):
         
         output = model(data)
         loss = loss_func(output.float(), target.float())
-        loss = torch.sqrt(2 * loss)
         
         loss.backward()
         optimizer.step()
@@ -44,18 +44,20 @@ def train(model, device, train_loader, optimizer, loss_func, epoch, model_name):
             print(target)
             print(output, '\n')
             
-        print('[epoch %d, batch_idx %2d] => average batch loss: %.2f' % (epoch+1, batch_idx, loss.item()))
+        print('[epoch %d, batch_idx %2d] => average datapoint and batch loss : %.2f' % (epoch+1, batch_idx, loss.item()))
         
         if batch_idx % 5 == 0:
-            with open(model_name + "_train.txt","a") as f_train:
+            path = os.path.join(results_folder, model_name + "_train.txt") 
+            with open(path, "a") as f_train:
                 f_train.write(str(loss.item()) + "\n")
     print('Finished training')
 
-
+    
+    
+    
 def evaluate(model, device, validation_loader, loss_func, model_name):
     model.eval()
     test_loss = 0
-    correct = 0
     with torch.no_grad():
         for batch_idx, (data, target) in enumerate(validation_loader):
             data = data.type(torch.float32).to(device)
@@ -63,7 +65,6 @@ def evaluate(model, device, validation_loader, loss_func, model_name):
             
             output = model(data)
             loss = loss_func(output.float(), target.float())
-            loss = torch.sqrt(2 * loss)
             test_loss += loss # sum up batch loss
             
             if batch_idx == 0:
@@ -74,19 +75,67 @@ def evaluate(model, device, validation_loader, loss_func, model_name):
     test_loss /= len(validation_loader.dataset)
     print('\nValidation set: Average loss: {:.4f}\n'.format(test_loss))
     
-    with open(model_name + "_valid.txt","a") as f_eval:
-        f_eval.write(str(test_loss.item()) + "\n")
+    path = os.path.join(results_folder, model_name + "_valid.txt") 
+    with open(path, "a") as f_valid:
+        f_valid.write(str(test_loss.item()) + "\n")
     
     return test_loss
 
+
+
+def test(model, device, test_loader, loss_func, model_name):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    print('Testing...')
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data = data.type(torch.float32).to(device)
+            target = target.to(device)
+            
+            output = model(data)
+            loss = loss_func(output.float(), target.float())
+            test_loss += loss # sum up batch loss
+            
+            target = target.numpy()[0]
+            output = output.numpy()[0]
+            
+            # Write out test labels and outputs 
+            
+            path = os.path.join(results_folder, model_name + "_test.txt") 
+            with open(path, "a") as f_test:
+                f_test.write(str(test_loss.item()) + "\n")
+                
+            path = os.path.join(results_folder, model_name + "_test_results.txt") 
+            with open(path,"a") as f_test:
+                f_test.write(str(loss.item()) + "\n")
+                f_test.write('[')
+                for i in range(len(target)):
+                    f_test.write(str(target[i]))
+                    if i < len(target) - 1:
+                        f_test.write(', ')
+                    
+                f_test.write('] \n')
+                
+                f_test.write('[')
+                for i in range(len(output)):
+                    f_test.write(str(output[i]))
+                    if i < len(output) - 1:
+                        f_test.write(', ')
+                    
+                f_test.write('] \n')
+                f_test.write('\n')
+                
+    print('Finished test prints')
+    return test_loss
 
 def main():
     # Training settings
     args = {
         "batch_size" : 2,
         "test_batch_size" : 1, 
-        "epochs" : 20, 
-        "gamma" : 0.07, 
+        "epochs" : 30, 
+        "gamma" : 0.1, 
         "log-interval" : 100,
         "lr" : 0.01, 
         "model_name" : "resnet_2_1d",
@@ -95,25 +144,22 @@ def main():
         "save-model" : False
     }
     
-    use_cuda = torch.cuda.is_available()
-    #use_cuda = False
     torch.manual_seed(args["seed"])
 
+    use_cuda = False #torch.cuda.is_available()
+    
     # CUDA for PyTorch
-    device = torch.device("cpu") # GPU if possible
-    
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    device = torch.device("cuda" if use_cuda else "cpu") # GPU if possible
+    print('Using device: ', device)
 
-    training_set = Dataset('train')
-    train_loader = data.DataLoader(training_set, batch_size=args['batch_size'], shuffle=True, num_workers=0)
+    training_set = SyntheticDataset('train', nb_of_input_images = 100)
+    train_loader = data.DataLoader(training_set, batch_size=args['batch_size'], shuffle=True, num_workers=4)
     
-    validation_set = Dataset('validation')
-    validation_loader = data.DataLoader(
-        validation_set, batch_size=args['batch_size'], shuffle=True, **kwargs
-    )
+    validation_set = SyntheticDataset('validation', nb_of_input_images = 100)
+    validation_loader = data.DataLoader(validation_set, batch_size=args['batch_size'], shuffle=True, num_workers=1)
     
-#     test_set = Dataset('test', dataset = dataset)
-#     test_loader = data.DataLoader(test_set, batch_size=1, shuffle=False, **kwargs)
+    test_set = SyntheticDataset('test', nb_of_input_images = 100)
+    test_loader = data.DataLoader(test_set, batch_size=args['test_batch_size'], shuffle=False)
     
     
 #     # get the model using our helper function
@@ -126,11 +172,10 @@ def main():
     model = get_model(args['model_name'])
     print('Using model : ', args['model_name'])
     
-    loss_func = nn.MSELoss(reduction='sum')
+    loss_func = nn.MSELoss(reduction='mean')
     
     # move model to the right device
     model.to(device)
-
     # construct an optimizer
     optimizer = optim.SGD(model.parameters(), lr=args['lr'])
     # and a learning rate scheduler
@@ -139,10 +184,11 @@ def main():
                                                     gamma=args['gamma'])
 
     num_epochs = args['epochs']
-
+    
 #     best_val_loss = float("inf")
 #     best_model = copy.deepcopy(model)
 #     best_epoch = 0
+    print('Training for :', num_epochs, 'epochs')
     for epoch in range(num_epochs):
         # train for one epoch, printing every 10 iterations
         train(model, device, train_loader, optimizer, loss_func, epoch, model_name)
@@ -154,6 +200,9 @@ def main():
 #             best_epoch = copy.deepcopy(epoch)
         lr_scheduler.step()
     
-
+    
+    test(model, device, test_loader, loss_func, model_name)
+    
+    
 if __name__ == '__main__':
     main()
