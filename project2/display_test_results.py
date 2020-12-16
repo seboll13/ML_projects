21aa3a6de7902c086576dd93af7977836742425a
seboll13 <seboll13@gmail.com>
2020-12-16 18:48:06 +0100

    commented display scripts

--- project2/display_test_results.py
+++ project2/display_test_results.py
@@ -1,8 +1,8 @@
-
 import matplotlib.pyplot as plt
 import os.path
 import numpy as np
 
+# parameters
 folder = 'models'
 model = 'resnet_3d_11'
 path = os.path.join(folder,model,"test_results.txt") 
@@ -13,11 +13,13 @@ outputs = []
 loss = []
 
 if __name__ == '__main__':
+    # go through each array of results
     with open(path,"r") as f:
         for line in f:
             l = [float(x) for x in line[1:-3].split(", ")]
             lines.append(l)
 
+    # add losses to its respective array
     with open(path_loss,"r") as f_l:
         for line in f_l:
             loss.append(float(line))
@@ -25,12 +27,14 @@ if __name__ == '__main__':
     loss = np.array(loss)
     labels, outputs = lines[::2], lines[1::2]
 
+    # compute and display useful calculations
     for i in range(len(loss)):
         print('Average loss for this prediction :', loss[i])
 
     print('Mean :', np.mean(loss))
     print('Std :', np.std(loss))
 
+    # handle plotting part
     for i in range(len(loss)):
         plt.figure()
         line_t, = plt.plot(labels[i], label="target")
@@ -40,6 +44,6 @@ if __name__ == '__main__':
         plt.ylabel('Velocity')
         plt.xlabel('Column')
         plt.legend(handles=[line_t, line_o])
-        #plt.show()
+
         plt.savefig(os.path.join(folder,model,'plot_test_results_' + str(i) + '.png'))
     
